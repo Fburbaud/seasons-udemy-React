ee9a6461d9193f8022d085be724d071c2ac8ab5c
Fburbaud <fred.brechet@gmail.com>
2022-01-16 19:51:35 +0100

    first conditionnal rendering

--- src/index.js
+++ src/index.js
@@ -15,16 +15,30 @@ import ReactDOM from "react-dom";
 class App extends React.Component{
     constructor(props){
         super(props);
-        //initialize state with lat and a default value
-        this.state = {lat: null};
+        //initialize state with lat and a default value and it's the only time
+        // we do direct assignment to this.state
+        this.state = {lat: null, errorMessage: ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //we called setState
+                this.setState({lat : position.coords.latitude});
+            }, //success callback
+            (err) => {
+                this.setState({errorMessage: err.message});
+            } //erreur callback
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), //success callback
-            (err) => console.log(err) //erreur callback
-        );
-        return <div>Latitude : {this.state.lat}</div>
+        //conditionnnal rendering
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading!</div>
     }
 }
 
