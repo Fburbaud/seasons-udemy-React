8750c5d581f74a5f47672e16964bf353189beb50
Fburbaud <fred.brechet@gmail.com>
2022-01-18 21:33:49 +0100

    seasonDisplay wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from './SeasonDisplay';
 //function component style:
 // const App = () => {
 //     window.navigator.geolocation.getCurrentPosition(
@@ -13,20 +13,18 @@ import ReactDOM from "react-dom";
 
 //class component style:
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        //initialize state with lat and a default value and it's the only time
-        // we do direct assignment to this.state
-        this.state = {lat: null, errorMessage: ''};
+    // constructor(props){
+    //     super(props);
+    //     //first method to initialize state with lat and a default value and it's the only time
+    //     // we do direct assignment to this.state
+    //     this.state = {lat: null, errorMessage: ''};
+    // }
+    state = {lat: null, errorMessage: ''};
 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //we called setState
-                this.setState({lat : position.coords.latitude});
-            }, //success callback
-            (err) => {
-                this.setState({errorMessage: err.message});
-            } //erreur callback
+            position => this.setState({lat : position.coords.latitude}), //success callback
+            err => this.setState({errorMessage: err.message})//erreur callback
         );
     }
 
@@ -36,7 +34,7 @@ class App extends React.Component{
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
         return <div>Loading!</div>
     }
